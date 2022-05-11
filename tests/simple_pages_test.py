592b870acedc0ae71d6cbc2c1dcd9644dbc3cf35
zheka <zvenglinski@gmail.com>
2022-05-11 06:55:12 -0400

    request register test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -21,13 +21,19 @@ def test_request_upload_transactions(client):
     assert response.status_code == 200
     assert b"accounts/transactions/upload" in response.data
 
-def test_request_page1(client):
+def test_request_register(client):
     """This makes the index page"""
-    response = client.get("/welcome")
+    response = client.get("/register")
     assert response.status_code == 200
-    assert b"welcome" in response.data
+    assert b"register" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
     response = client.get("/page5")
     assert response.status_code == 404
+
+    def test_request_register(client):
+        """This makes the index page"""
+        response = client.get("/register")
+        assert response.status_code == 200
+        assert b"register" in response.data
