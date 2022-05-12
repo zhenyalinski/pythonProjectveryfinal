97dc47e77e2b14b6f159fce468f470a32e8f5e7f
zheka <zvenglinski@gmail.com>
2022-05-11 20:38:48 -0400

    auth_pages_register_login test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -10,8 +10,21 @@ def test_request_auth_main_menu_links(client):
     response = client.get("/")
 
     log = logging.getLogger("upload")
-    log.info("inside auth_test / test request main menu link")
+    log.info("equest main menu link in auth pages")
 
     assert response.status_code == 200
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
+
+def test_auth_pages_register_login(client):
+    """This makes the index page"""
+    response = client.get("/dashboard")
+
+    log = logging.getLogger("upload")
+    log.info("test_auth_pages register + login")
+
+    response = client.get("/register")
+    assert response.status_code == 200
+    response = client.get("/login")
+    assert response.status_code == 200
