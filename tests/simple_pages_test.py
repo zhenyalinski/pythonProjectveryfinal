601f79d14933d620e882546f4d339be2a52f0e5b
zheka <zvenglinski@gmail.com>
2022-05-11 21:34:53 -0400

    test_request_home test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -60,6 +60,10 @@ def test_request_main_menu_links(client):
 #     assert b"transactions" in response.data
 
 
-
+def test_request_home(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Home" in response.data
 
 
