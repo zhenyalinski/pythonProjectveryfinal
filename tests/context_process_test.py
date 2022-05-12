1c64ea002504ee32463be920b611738d7d9799eb
zheka <zvenglinski@gmail.com>
2022-05-12 00:46:22 -0400

    last touch

--- tests/context_process_test.py
+++ tests/context_process_test.py
@@ -8,6 +8,7 @@ def test_context_variables_environment(client):
     env = getenv('FLASK_ENV', None)
     test_string = f"Environment: {env}"
     content = bytes(test_string, 'utf-8')
+
     assert response.status_code == 200
     assert content in response.data
 
@@ -29,6 +30,7 @@ def test_context_variables_year(client):
     year = date.strftime("%Y")
     test_string = f"Copyright: {year}"
     content = bytes(test_string, 'utf-8')
+
     assert response.status_code == 200
     assert content in response.data
 
@@ -37,5 +39,6 @@ def test_context_currency_format(client):
     response = client.get("/")
     test_string = f"$100"
     content = bytes(test_string, 'utf-8')
+
     assert response.status_code == 200
     assert content in response.data
